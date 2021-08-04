2ef2848613bc60f6b77447fb18f44c289faabcc2
zsvs <stepanezc@gmail.com>
2021-08-04 17:52:52 +0300

    Corrected providet section

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "github" {}
-
 resource "github_repository" "new_repo" {
   name        = var.git_repo_name
   description = var.git_repo_description
